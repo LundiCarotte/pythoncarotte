a350529203f6b09d8fa3b059402e9ff942e9e480
Aurélie Valery <aurelie.valery@gmail.com>
2020-05-31 20:35:55 +0200

    refacto main.py to improve code

--- main.py
+++ main.py
@@ -11,29 +11,23 @@ arguments = sys.argv[1:]
 if len(arguments) == 0:
     print("ERREUR : nom de fichier manquant.")
     print("Essayez plutôt : 'python main.py nom_du_fichier [arguments optionnels]")
+    exit()
 
-elif len(arguments) == 1:
-    fileName = arguments[0]
-    
-    if (not os.path.isfile(fileName)):
-        print("ERREUR : le fichier '{0}' n'existe pas".format(fileName))
-        exit()
-    
-    mainfunc.txtenhtml(fileName, "hebdomadaire")
+if len(arguments) > 2:
+    print("ERREUR: n'entrez pas plus de 2 arguments svp :)")
+    exit()
 
-elif len(arguments) == 2:
-    fileName = arguments[0]
+fileName = arguments[0]
+if (not os.path.isfile(fileName)):
+    print("ERREUR : le fichier '{0}' n'existe pas".format(fileName))
+    exit()
+
+format = "hebdomadaire"
+if len(arguments) == 2:
     format = arguments[1]
-    
-    if (not os.path.isfile(fileName)):
-        print("ERREUR : le fichier '{0}' n'existe pas".format(fileName))
-        exit()
-
     if (not format in ["artsup", "minimail"]):
         print("ERREUR: format de fichier '{0}' invalide.".format(format))
         print("Formats autorisés: artsup, minimail.")
-    else:
-        mainfunc.txtenhtml(fileName, format)
+        exit()
 
-elif len(arguments) > 2:
-    print("ERREUR: n'entrez pas plus de 2 arguments svp :)")
+mainfunc.txtenhtml(fileName, format)
